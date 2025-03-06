3f8413c2329437d9b97c5b7bb886a795f1576b9a
Zerubabel <zerucodes@gmail.com>
2025-03-06 20:47:41 +0300

    db configured

---
+++ server/dbConfig/dbConfig.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const MONGO_URI = process.env.MONGO_URI || "";
+
+const dbConfig = async () => {
+  try {
+    await mongoose.connect(MONGO_URI);
+    console.log("MongoDB connected successfully");
+  } catch (error) {
+    console.error("MongoDB connection error:", error);
+    process.exit(1); // Exit the process if the connection fails
+  }
+};
+
+module.exports = { dbConfig };
