fd98eac7e44a74d3a825e690f4de10aa396fe60d
Zerubabel <zerucodes@gmail.com>
2025-03-07 09:47:44 +0300

    footer updated

---
+++ client/src/components/Footer.tsx
@@ -0,0 +1,25 @@
+const Footer = () => {
+  return (
+    <footer className="bg-blue-600 p-4 text-white p-10 w-full">
+      <div className="flex justify-around">
+        <aside>
+          <p>
+            Managing Leads
+            <br />
+            Providing Solutions
+          </p>
+        </aside>
+        <nav>
+          <h6 className="text-lg font-bold mb-2">Social Media</h6>
+          <div className="grid grid-flow-col gap-4">
+            <a href="#">🔗</a>
+            <a href="#">🔗</a>
+            <a href="#">🔗</a>
+          </div>
+        </nav>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
