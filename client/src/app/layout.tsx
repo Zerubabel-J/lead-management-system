e7eb465b9be17331fe3494ca989e7f9e71ece8c4
Zerubabel <zerucodes@gmail.com>
2025-03-07 09:48:13 +0300

    layout configured

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,13 +1,22 @@
 import "./globals.css";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
 
 export default function RootLayout({
   children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+}: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en">
-      <body suppressHydrationWarning={true}>{children}</body>
+      <body
+        suppressHydrationWarning={true}
+        className="flex flex-col min-h-screen"
+      >
+        <Navbar />
+        <main className="flex-1 flex flex-col justify-center items-center">
+          {children}
+        </main>
+        <Footer />
+      </body>
     </html>
   );
 }
